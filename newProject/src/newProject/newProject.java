c1245351be65bf536dfca0cbc8e84b4e045a8e68
ideastudent <ideastudent@10.235.65.9>
2017-10-11 17:31:10 -0700

    merp

---
+++ newProject/src/newProject/newProject.java
@@ -0,0 +1,18 @@
+package newProject;
+
+import java.util.Scanner;
+
+public class newProject {
+
+public static void main(String []args) {
+	Scanner newScanner = new Scanner(System.in);
+	String newString;
+	newString = newScanner.nextLine();
+	if(newString == newScanner.nextLine()) {
+		System.out.println("Congrats!");
+		
+	} 
+	
+}
+}
+
